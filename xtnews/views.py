dc01f25f7fdb4a6634db45f90078fbb091c32669
kubus <kubus@kubus-work.(none)>
2013-09-09 15:55:16 +0400

    view.self.db

--- xtnews/views.py
+++ xtnews/views.py
@@ -11,17 +11,18 @@ class BaseView(object):
     def __init__(self, request):
         self.request = request
         self.request.response.content_type = 'application/xml'
+        self.db = models.DBSession()
 
 
 class Collection(BaseView):
 
     def get_object_list(self):
-        return models.DBSession.query(models.News).all()
+        return self.db.query(models.News).all()
 
     @view_config(route_name='list', renderer='object_list.xml.jinja2', request_method='GET')
     def get(self):
         return {
-            'object_list': ( x.as_dict() for x in self.get_object_list())
+            'object_list': (x.as_dict() for x in self.get_object_list())
         }
 
     @view_config(route_name='list', renderer='object_form_errors.xml.jinja2', request_method='POST')
@@ -32,14 +33,15 @@ class Collection(BaseView):
             return {'form': form}
         obj = models.News()
         form.populate_obj(obj)
-        models.DBSession.add(obj)
+        self.db.add(obj)
         transaction.commit()
         return HTTPCreated()
+
 
 class Item(BaseView):
 
     def get_object(self, object_id):
-        obj = models.DBSession.query(models.News).get(object_id)
+        obj = self.db.query(models.News).get(object_id)
         if not obj:
             raise NotFound("Object with id %s not found" % object_id)
         return obj
@@ -54,7 +56,7 @@ class Item(BaseView):
     @view_config(route_name='item', renderer='object.xml.jinja2', request_method='DELETE')
     def delete(self):
         obj = self.get_object(self.request.matchdict['id'])
-        models.DBSession.delete(obj)
+        self.db.delete(obj)
         return HTTPNoContent()
 
     @view_config(route_name='item', renderer='object_form_errors.xml.jinja2', request_method='PUT')
@@ -65,6 +67,6 @@ class Item(BaseView):
             return {'form': form}
         obj = self.get_object(self.request.matchdict['id'])
         form.populate_obj(obj)
-        models.DBSession.add(obj)
+        self.db.add(obj)
         transaction.commit()
         return HTTPNoContent()
